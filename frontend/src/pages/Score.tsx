a742c987983e9d1a7ba2c7a6aa45589b380094b2
Rohit Nair <nairrohit003@gmail.com>
2023-11-11 14:37:34 -0700

    Modified bar to be responsive based on score.

--- frontend/src/pages/Score.tsx
+++ frontend/src/pages/Score.tsx
@@ -1,17 +1,38 @@
-import { useState } from 'react'
-import './Score.css'
+// Score.tsx
+import React from 'react';
+import LinearProgress from '@mui/material/LinearProgress';
 
-function App() {
-  const [score, setScore] = useState(0)
-
-  return (
-    <>
-      <div>
-        <LinearProgress variant="determinate" value={score} />
-      </div>
-
-    </>
-  )
+interface ScoreBarProps {
+  score: number;
 }
 
-export default App
+const ScoreBar: React.FC<ScoreBarProps> = ({ score }) => {
+  const progress = Math.min(100, Math.max(0, score));
+  
+  let isBelow50 = true;
+  let isBelow75 = true;
+
+  if (progress <= 50) {
+    isBelow50 = true;
+    isBelow75 = true
+  } else if (progress <= 75) {
+    isBelow50 = false;
+    isBelow75 = true;
+  }
+  else{
+    isBelow50 = false;
+    isBelow75 = false;
+  }
+
+  return (
+    <div className="score-bar">
+      <LinearProgress      
+        color={isBelow50 ? 'error' : (isBelow75 ? 'warning' : 'success')}
+        variant="determinate"
+        value={progress}
+      />
+  </div>
+  );
+};
+
+export default ScoreBar;
