b4851550f5d9f8e0a0a244c22bef2f8e97d69399
Douglas Yau <56883284+digug@users.noreply.github.com>
2023-11-12 03:24:42 -0700

    Create and import About page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,20 @@
-import { useState } from 'react'
-import {Routes, Route} from 'react-router-dom'
-import ScorePage from '../src/pages/ScorePage.tsx';
-import './App.css'
-import Layout from './pages/Layout/Layout'
-import UploadPortfolio from './pages/UploadPortfolio.tsx'
-import HomePage from './pages/Homepage.tsx'; 
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./pages/About.tsx";
+import HomePage from "./pages/Homepage.tsx";
+import Layout from "./pages/Layout/Layout";
+import UploadPortfolio from "./pages/UploadPortfolio.tsx";
+
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout/>}>         
-        <Route index element={<HomePage/>}/>  
-        {/*<Route path="/about" element={<About/>}/>
-        */}
-        <Route path="/evaluate" element={<UploadPortfolio/>}/> 
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/about" element={<About />} />
 
-
+        <Route path="/evaluate" element={<UploadPortfolio />} />
       </Route>
     </Routes>
-  )
+  );
 }
 export default App;
---
+++ frontend/src/pages/About.tsx
@@ -0,0 +1,26 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>
+        Welcome to Team Manchester United, where innovation meets
+        sustainability! We are a passionate team of four with expertise from
+        various disciplines, including engineering, computer science, and
+        mathematics. We are excited to present our groundbreaking project – an
+        eco-friendly financial portfolio analyzer.
+      </p>
+      <h2>Mission Statement:</h2>
+      <p>
+        {" "}
+        We recognize the pressing need for corporation to operate responsibly
+        and with sustainability as priority. Our solution aims to address this
+        by reimagining the traditional financial portfolio in an eco-friendly
+        light. We understand that responsible investing is not just a choice;
+        it's a commitment to a better world. We are committed to help investors
+        realize that they can make the world a better place for everyone.
+      </p>
+    </div>
+  );
+};
+
+export default About;
