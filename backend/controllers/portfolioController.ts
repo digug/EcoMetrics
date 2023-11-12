d2e63e09d6c4dfeaacc516dc91f9ccc162cef0b4
Nishan Soni <nishan.soni2@gmail.com>
2023-11-11 21:15:51 -0700

    finish

--- backend/controllers/portfolioController.ts
+++ backend/controllers/portfolioController.ts
@@ -1,39 +1,93 @@
 import axios from "axios";
 
+const GDELT_URL = "https://api.gdeltproject.org/api/v2/doc/doc";
+const TONECHART_MODE = "tonechart";
+const TIMESPAN = "48m";
+
+export const environment = {
+  keywords: "(waste OR sustain OR environment OR energy)",
+  theme: "WB_1786_ENVIRONMENTAL_SUSTAINABILITY",
+};
+
+export const social = {
+  keywords: "(abuse OR emotional)",
+  theme: "HUMAN_RIGHTS_ABUSES",
+};
+
+export const governance = {
+  keywords: "(ethics OR layoff OR management)",
+  theme: "WB_725_ORGANIZATIONAL_MANAGEMENT",
+};
+
 /**
- * Gets all articles within the last year with their article title and tone
- * @param company The company name
+ * Calculate the tone for a company and its category (E S or G)
+ * @param company
+ * @returns Array of tones
  */
-export const getAllArticles = async (company: string) => {
-  const GDELT_URL = "https://api.gdeltproject.org/api/v2/doc/doc";
-  const TIMELINE_TONE_MODE = "timelinetone";
-  const ART_LIST_MODE = "artlist";
-  const TIMESPAN = "12m";
-  const DATE_DESC = "DateDesc";
-
-  let all_articles = [];
-
-  const timeline_tone = await axios.get(GDELT_URL, {
+export const getAverageESGTone = async (company: string, category: any) => {
+  let tones = await axios.get(GDELT_URL, {
     params: {
-      query: company,
+      query: `${company} ${category.keywords}`,
       format: "json",
-      mode: TIMELINE_TONE_MODE,
+      mode: TONECHART_MODE,
       timespan: TIMESPAN,
-      sort: DATE_DESC,
+      theme: category.theme,
+      sourcelang: "eng",
     },
   });
+  return calculateAvgTone(process(tones.data.tonechart, company));
+};
 
-  const art_list = await axios.get(GDELT_URL, {
-    params: {
-      query: company,
-      format: "json",
-      mode: ART_LIST_MODE,
-      timespan: TIMESPAN,
-      sort: DATE_DESC,
-    },
+/**
+ * Proccess array
+ * @param data
+ * @param company
+ * @returns proccessed array
+ */
+const process = (data: any[], company: string) => {
+  let ans: any[] = [];
+  data.forEach((element: any, index: number) => {
+    element.toparts.forEach((article: any) => {
+      let title: string = article.title;
+      if (title.toLowerCase().includes(company.toLowerCase())) {
+        ans.push(element.bin);
+      }
+    });
   });
+  return ans;
+};
 
-  //   for (let i = 0; i < art_list.data.length(); ) {
+/**
+ * Calculate the average tone of a tone array
+ * @param tone_arr
+ * @returns avg tone of the array
+ */
+const calculateAvgTone = (tone_arr: any[]) => {
+  if (tone_arr.length == 0) {
+    console.log(0);
+    return 0;
+  }
+  let avg =
+    tone_arr.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue;
+    }, 0) / tone_arr.length;
+  console.log(avg);
+  return avg;
+};
 
-  //   }
+const evaluateCompany = async (company: string) => {
+  const e = await getAverageESGTone(company, environment);
+  const s = await getAverageESGTone(company, social);
+  const g = await getAverageESGTone(company, governance);
+  return [e, s, g];
+};
+
+/**
+ * Evaluate the portfolio
+ * @param percentages
+ * @param companies
+ */
+export const evaluatePortfolio = (percentages: any[], companies: any[]) => {
+  const evaluation = {};
+  //   companies.forEach();
 };
