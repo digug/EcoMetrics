68bf9b1560dac47b15ab14b15f1dbec5ffcef9ea
Nishan Soni <nishan.soni2@gmail.com>
2023-11-12 10:30:54 -0700

    c

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -3,47 +3,23 @@ import demo from '../assets/Screenshot_2023-11-12_at_2.04.25_AM-removebg.png';
 import stockWorld from '../assets/pexels-hatice-baran-17931015.jpg';
 import '../styles/Homepage.css';
 import { Button } from '@mui/material';
+import '@fontsource/open-sans/300-italic.css'
+import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
+  const navigate = useNavigate()
   return (
-    <div className="all-container">
-      <div className="home-page-container">
-        <img className="demo-image" src={demo} alt="Platform Screenshot" />
-        <div className="text-container">
-          <h1>Sustainable Investing is Important...</h1>
-          <p></p>
-          <p>
-            Using our tool can help you <strong>own your investments.</strong> Get started today.
-          </p>
-          <p></p>
-          <Button variant="contained">Evaluate Portfolio</Button>
+    <div style={{display: 'flex', flexDirection:'column', alignItems:'center', textAlign: 'center'}}>
+      <div>
+        <div style={{fontSize: '4.2rem'}}>
+          Redefining <span style = {{textDecoration: 'underline', fontStyle: 'italic', textDecorationColor: '#53CA61', textUnderlineOffset: '1rem'}}>Sustainable</span> Investing<span style={{color: '#53CA61'}}>.</span>
         </div>
-      </div>
-
-      <div className="bullets-container">
-        <div className="text-container">
-          <h1>We offer an easy-to-use solution that gives you peace of mind over your investments:</h1>
-          <p></p>
-          <ul className='ul-spacing'>
-            <li>Get the ESG scores of all NASDAQ and NYSE companies</li>
-            <li>See a comprehensive ESG breakdown</li>
-            <li>Explore industry-specific ESG ratings</li>
-            <li>Compare ESG performance across different sectors</li>
-            <li>Access historical ESG data for trend analysis</li>
-            <li>Receive personalized ESG investment recommendations</li>
-            <li>Discover socially responsible investment opportunities</li>
-            <li>Learn about the methodology behind our ESG scoring</li>
-          </ul>
-          <p></p>
-          <Button variant="contained">About Us</Button>
+        <div style={{marginTop: '1.2rem'}}>
+          <div style={{fontSize:'1.3rem'}}>
+            Ecometrics presents a comprehensive analysis, offering deep insight into the sustainability of your investment portfolio<span style={{color: '#53CA61'}}>.</span>
+          </div>
         </div>
-        <div className="image-container">
-          <img className="world-image" src={stockWorld} alt="World Image" />
-        </div>
-      </div>
-
-      <div className="footer">
-        &copy; PECOM 2023
+        <button onClick={() => {navigate("/evaluate")}} style = {{borderRadius: '5px', backgroundColor: '#35AC43', marginTop: '3rem', fontSize: '1.2rem', paddingBlock: '0.8'}}>Get Started</button>
       </div>
     </div>
   );
